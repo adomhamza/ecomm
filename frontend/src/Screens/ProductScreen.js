7aa3c00d89b603d8c5fb6b00dddf56f28788e2b1
adomhamza <liveghost93@gmail.com>
2020-05-27 01:32:52 +0000

    Update

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -5,14 +5,47 @@ function ProductScreen(props) {
   const product = data.products.find((x) => x._id === props.match.params.id);
   return (
     <div>
-      <div>
+      <div className="back-to-result">
         <Link to="/">Back</Link>
       </div>
       <div className="details">
         <div className="details-image">
           <img src={product.image} alt="product-image"></img>
         </div>
-        <div className="details-info"></div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+            <li>
+              {product.rating} Stars {product.numReviews}
+            </li>
+            <li>
+              Price:&#8373;<b>{product.price}</b>
+            </li>
+            <li>
+              Description
+              <div>{product.description}</div>
+            </li>
+          </ul>
+        </div>
+        <div className="details-action">
+          <ul>
+            <li>Price:{product.price}</li>
+            <li>Status:{product.price}</li>
+            <li>
+              Qty:
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+            </li>
+            <li>
+              <button>Add to Cart</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
